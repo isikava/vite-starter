60952e4eb01aba24b44a85c2688df9e13272470a
Alexander Ignatev <alexign473@gmail.com>
2023-08-16 09:58:10 +0300

    Add Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
