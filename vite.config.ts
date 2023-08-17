2459fc2cdff6242769904c04365605fe4d06304e
Alexander Ignatev <alexign473@gmail.com>
2023-08-17 18:24:02 +0300

    Absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
